6c3191d299c5e0e86c98425c6a9af196350ebc2c
Shumon Ahammed <sumonmahamud1998@gmail.com>
2023-08-02 13:58:24 +0600

    single post page

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -38,11 +38,12 @@ class Menus {
 
   	public function get_menu_id( $location )
   	{
-  		// Get all the locations.
   		$locations = get_nav_menu_locations();
-  		// Get object id by location.
-  		$menu_id = $locations[$location];
-  		return ! empty( $menu_id ) ? $menu_id: '';
+
+        // Get object id by location.
+        $menu_id = ! empty($locations[$location]) ? $locations[$location] : '';
+
+        return ! empty( $menu_id ) ? $menu_id : '';
   	}
   	public function get_child_menu_items( $menu_array, $parent_id )
   	{
