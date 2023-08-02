9a2f58fd05f1ace997a6853207e38e74aecaa959
Shumon Ahammed <sumonmahamud1998@gmail.com>
2023-08-02 12:47:59 +0600

    front page

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php 
+/**
+ * Front page template
+ * 
+ * @package Finanza
+ * 
+**/
+
+get_header();
+?>
+
+<!-- Page Header Start -->
+    <div class="container-fluid page-header mb-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container">
+            <h1 class="display-3 mb-4 animated slideInDown">Front Page</h1>
+        </div>
+    </div>
+    <!-- Page Header End -->
+
+
+<?php get_footer(); ?>
